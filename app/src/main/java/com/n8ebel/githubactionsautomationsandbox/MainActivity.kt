6a25c0bc29906ad63ffc3ef94d32fb33005dcd74
Nate Ebel <n8ebel@gmail.com>
2019-11-05 22:33:26 +0100

    Another failing commit

--- app/src/main/java/com/n8ebel/githubactionsautomationsandbox/MainActivity.kt
+++ app/src/main/java/com/n8ebel/githubactionsautomationsandbox/MainActivity.kt
@@ -3,7 +3,7 @@ package com.n8ebel.githubactionsautomationsandbox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity  AppCompatActivity() {
+class MainActivity AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
