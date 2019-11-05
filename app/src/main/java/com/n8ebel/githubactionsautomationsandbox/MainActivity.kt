5496f8f6da7daa82abd6d131b3e1a227df5895cf
Nate Ebel <n8ebel@gmail.com>
2019-11-06 00:06:49 +0100

    Fix errors

--- app/src/main/java/com/n8ebel/githubactionsautomationsandbox/MainActivity.kt
+++ app/src/main/java/com/n8ebel/githubactionsautomationsandbox/MainActivity.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
