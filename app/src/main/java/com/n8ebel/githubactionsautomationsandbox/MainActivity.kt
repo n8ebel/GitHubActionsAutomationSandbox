f0b5e648525bde900e6bfc1ca5c7adffe96d1c8f
Nate Ebel <n8ebel@gmail.com>
2019-11-05 23:01:53 +0100

    Fail build because of ktlint

--- app/src/main/java/com/n8ebel/githubactionsautomationsandbox/MainActivity.kt
+++ app/src/main/java/com/n8ebel/githubactionsautomationsandbox/MainActivity.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
