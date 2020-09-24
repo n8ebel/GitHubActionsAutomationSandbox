bdab366301c8a729b309975b8e4960f1b210d974
Nate Ebel <n8ebel@goobar.io>
2020-09-24 04:15:39 -0700

    Enable build cache and UI tests

--- app/src/androidTest/java/com/n8ebel/githubactionsautomationsandbox/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/n8ebel/githubactionsautomationsandbox/ExampleInstrumentedTest.kt
@@ -3,6 +3,7 @@ package com.n8ebel.githubactionsautomationsandbox
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.runner.RunWith
 import org.junit.Test
 
@@ -13,6 +14,13 @@ import org.junit.Test
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext_WillFail() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertNotEquals("com.n8ebel.githubactionsautomationsandbox", appContext.packageName)
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
